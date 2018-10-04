5a58aee7eeeef5457d6a5a8bbcaddfb1bd2f7117
zzzonked <yaroslavkuts@gmail.com>
2018-10-04 21:58:36 +0300

    SQL query for searching amount of medals was corrected.

--- data.js
+++ data.js
@@ -18,19 +18,19 @@ var getNOCs = function (callback) {
 var getAmountOfMedals = function (season, noc, medal, callback) {
   let db = new sqlite3.Database('./olympic_history.db');
 
-  medal = medal ? `and r.medal = ${medal}` : ``;
+  medal = medal ? `= ${medal}` : ` > 0`;
 
   let sql = `select year Year, res.num Amount
-             from games left outer join
-               (select g.id id, count(r.medal) num from games g
-               join results r on (g.id = r.game_id)
-               join athletes a on (r.athlete_id = a.id)
-               join teams t on (a.team_id = t.id)
-               where season = ${season}
-               and t.noc_name = '${noc}'
-               ${medal}
-               group by g.year) res
-             on (games.id = res.id) order by year`;
+            from games left outer join
+              (select g.id, count(r.medal) num from results r
+              join games g on (r.game_id = g.id)
+              join athletes a on (r.athlete_id = a.id)
+              join teams t on (a.team_id = t.id)
+              where g.season = ${season}
+              and t.noc_name = '${noc}'
+              and r.medal ${medal}
+              group by g.year) res
+            on (games.id = res.id) order by year`;
 
   db.all(sql, [], (err, rows) => {
     if (err) {
