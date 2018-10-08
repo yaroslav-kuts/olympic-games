e2e96c184f43c41bcbb73c445c5b9b831d6408ad
zzzonked <yaroslavkuts@gmail.com>
2018-10-08 15:11:30 +0300

    'data.js' was renamed to 'db.js' as this is a more appropriate name.

--- import.js
+++ import.js
@@ -1,57 +1,57 @@
 const queries = require('./libs/queries');
-const data = require('./libs/data');
+const db = require('./libs/db');
 
-const dropGamesIndex = data.run(queries.dropGamesIndex, `Games index has been droped.`);
+const dropGamesIndex = db.run(queries.dropGamesIndex, `Games index has been droped.`);
 
-const dropSportsIndex = data.run(queries.dropSportsIndex, `Sports index has been droped.`);
+const dropSportsIndex = db.run(queries.dropSportsIndex, `Sports index has been droped.`);
 
-const dropEventsIndex = data.run(queries.dropEventsIndex, `Events index has been droped.`);
+const dropEventsIndex = db.run(queries.dropEventsIndex, `Events index has been droped.`);
 
-const dropAthletesIndex = data.run(queries.dropAthletesIndex, `Athletes index has been droped.`);
+const dropAthletesIndex = db.run(queries.dropAthletesIndex, `Athletes index has been droped.`);
 
-const castSeasonToEnum = data.run(queries.seasonToEnumQuery, `Season column of temp table casted to enum.`);
+const castSeasonToEnum = db.run(queries.seasonToEnumQuery, `Season column of temp table casted to enum.`);
 
-const createGamesIndex = data.run(queries.createGamesIndexQuery, `Index in games table was created.`);
+const createGamesIndex = db.run(queries.createGamesIndexQuery, `Index in games table was created.`);
 
-const createEventsIndex = data.run(queries.createEventsIndexQuery, `Index in games table was created.`);
+const createEventsIndex = db.run(queries.createEventsIndexQuery, `Index in games table was created.`);
 
-const createSportsIndex = data.run(queries.createSportsIndexQuery, `Index in sports table was created.`);
+const createSportsIndex = db.run(queries.createSportsIndexQuery, `Index in sports table was created.`);
 
-const createAthletesIndex = data.run(queries.createAthletesIndexQuery, `Index in athletes table was created.`);
+const createAthletesIndex = db.run(queries.createAthletesIndexQuery, `Index in athletes table was created.`);
 
-const removeUnofficialYearRecords = data.run(queries.removeUnofficialYearRecordsQuery, `Records with 1906 year were truncated.`);
+const removeUnofficialYearRecords = db.run(queries.removeUnofficialYearRecordsQuery, `Records with 1906 year were truncated.`);
 
-const cleanSports = data.run(queries.cleanSportsTable, `'Sports' table was truncated.`);
+const cleanSports = db.run(queries.cleanSportsTable, `'Sports' table was truncated.`);
 
-const cleanEvents = data.run(`delete from events`, `'Events' table was truncated.`);
+const cleanEvents = db.run(`delete from events`, `'Events' table was truncated.`);
 
-const cleanTeams = data.run(`delete from teams`, `'Teams' table was truncated.`);
+const cleanTeams = db.run(`delete from teams`, `'Teams' table was truncated.`);
 
-const cleanAthletes = data.run(`delete from athletes`, `'Athletes' table was truncated.`);
+const cleanAthletes = db.run(`delete from athletes`, `'Athletes' table was truncated.`);
 
-const cleanGames = data.run(`delete from games`, `'Games' table was truncated.`);
+const cleanGames = db.run(`delete from games`, `'Games' table was truncated.`);
 
-const cleanResults = data.run(`delete from results`, `'Results' table was truncated.`);
+const cleanResults = db.run(`delete from results`, `'Results' table was truncated.`);
 
-const fillSportsTable = data.run(queries.fillSportsQuery, `'Sports' table has been fulfilled.`);
+const fillSportsTable = db.run(queries.fillSportsQuery, `'Sports' table has been fulfilled.`);
 
-const fillEventsTable = data.run(queries.fillEventsQuery, `'Events' table has been fulfilled.`);
+const fillEventsTable = db.run(queries.fillEventsQuery, `'Events' table has been fulfilled.`);
 
-const fillTeamsTable = data.run(queries.fillTeamsQuery, `'Teams' table has been fulfilled.`);
+const fillTeamsTable = db.run(queries.fillTeamsQuery, `'Teams' table has been fulfilled.`);
 
-const fillGamesTable = data.run(queries.fillGamesQuery, `'Games' table has been fulfilled.`);
+const fillGamesTable = db.run(queries.fillGamesQuery, `'Games' table has been fulfilled.`);
 
-const resolveMultiCityProblem = data.run(queries.multiCityProblemQuery, `Multi city problem has been resolved.`);
+const resolveMultiCityProblem = db.run(queries.multiCityProblemQuery, `Multi city problem has been resolved.`);
 
-const removeDuplicatesGames = data.run(queries.removeDuplicatesQuery, `Duplicate games has been removed.`);
+const removeDuplicatesGames = db.run(queries.removeDuplicatesQuery, `Duplicate games has been removed.`);
 
-const fillResulsTable = data.run(queries.fillResulsQuery, `'Results' table has been fullfilled.`);
+const fillResulsTable = db.run(queries.fillResulsQuery, `'Results' table has been fullfilled.`);
 
-const fillAthletesTable = data.run(queries.fillAthletesQuery, `'Athletes' table has been fulfilled.`);
+const fillAthletesTable = db.run(queries.fillAthletesQuery, `'Athletes' table has been fulfilled.`);
 
-const removeTemp = data.run(`drop table temp`, `'Temp' table was removed.`);
+const removeTemp = db.run(`drop table temp`, `'Temp' table was removed.`);
 
-const trimAthleteNames = data.run(queries.trimAthleteNamesQuery, `Athlete names were trimmed.`);
+const trimAthleteNames = db.run(queries.trimAthleteNamesQuery, `Athlete names were trimmed.`);
 
 cleanResults()
   .then(cleanGames)
@@ -80,12 +80,12 @@ cleanResults()
   .then(removeTemp)
   .then(trimAthleteNames)
   .then(() => {
-    data.close(null, () => {
+    db.close(null, () => {
       console.log('Data was imported to DB.');
     })
   })
   .catch((err) => {
-    data.close(err, () => {
+    db.close(err, () => {
       removeTemp().then(() => {
         console.log('Data importing was interrupted by error.');
       });
--- stat.js
+++ stat.js
@@ -1,9 +1,9 @@
-const data = require('./libs/data');
+const db = require('./libs/db');
 const chart = require('./libs/chart');
 const queries = require('./libs/queries');
 
 const show = async function (args) {
-  const getTeamsPromise = data.all(`select id, noc_name from teams`);
+  const getTeamsPromise = db.all(`select id, noc_name from teams`);
   const teams = await getTeamsPromise();
   const seasons = { 'winter': 1, 'summer': 0 };
   const medals = { 'gold': 1, 'silver': 2, 'bronze': 3 };
@@ -29,13 +29,13 @@ const show = async function (args) {
   if (season === undefined) throw new Error('You have to specify season param!');
 
   if (noc) {
-    const getAmountPromise = data.all(queries.getAmountOfMedalsQuery(season, noc, medal));
+    const getAmountPromise = db.all(queries.getAmountOfMedalsQuery(season, noc, medal));
     let rows = await getAmountPromise();
     rows = rows.map(row => [row.Year, row.Amount || 0]);
     chart.draw(rows, 'medals', ['Year', 'Amount']);
   }
   else {
-    const getTopPromise = data.all(queries.getTopTeamsQuery(season, year, medal));
+    const getTopPromise = db.all(queries.getTopTeamsQuery(season, year, medal));
     let rows = await getTopPromise();
     rows = rows.map(row => [row.NOC, row.Amount || 0]);
     chart.draw(rows, 'top-teams', ['NOC', 'Amount']);
@@ -44,8 +44,8 @@ const show = async function (args) {
 
 var main = function () {
   show(process.argv.slice(2))
-  .then(() => { data.close(); })
-  .catch((err) => { data.close(err); });
+  .then(() => { db.close(); })
+  .catch((err) => { db.close(err); });
 }
 
 main();
